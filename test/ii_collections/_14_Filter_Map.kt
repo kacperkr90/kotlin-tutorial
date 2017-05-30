f27658690e81838f0a6f1a67fac08803be5e3559
Kacper Krycki <kacper.krycki@bms.com.pl>
2017-05-30 09:55:47 +0200

    Collections 15 - 17

--- test/ii_collections/_14_Filter_Map.kt
+++ test/ii_collections/_14_Filter_Map.kt
@@ -3,9 +3,7 @@ package ii_collections
 import ii_collections.data.*
 import org.junit.Assert.assertEquals
 import org.junit.Test
-gst
-gst
-gst
+
 class _14_Filter_Map {
     @Test fun testCitiesCustomersAreFrom() {
         assertEquals(setOf(Canberra, Vancouver, Budapest, Ankara, Tokyo), shop.getCitiesCustomersAreFrom())
