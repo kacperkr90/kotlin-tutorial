90d3d46407dafcdb1f11349bf80e36efb664ddde
Kacper Krycki <kacper.krycki@gmail.com>
2017-06-01 00:22:22 +0200

    Conventions 25 done

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,16 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return this.days().compareTo(other.days())
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+
+fun MyDate.days(): Int {
+    return year * 365 + month * 31 + dayOfMonth
+}
 
 enum class TimeInterval {
     DAY,
