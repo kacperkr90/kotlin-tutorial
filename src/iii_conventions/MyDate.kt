d3468d8061c75691260d5a250d01e93c5120e985
Kacper Krycki <kacper.krycki@gmail.com>
2017-06-03 23:32:02 +0200

    Conventions 28 - 30

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -6,7 +6,24 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
     }
 }
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange =
+        DateRange(this, other)
+
+operator fun MyDate.inc(): MyDate {
+    return this.nextDay()
+}
+
+operator fun MyDate.plus(interval: TimeInterval): MyDate =
+        when (interval) {
+            TimeInterval.DAY -> this.nextDay()
+            TimeInterval.WEEK -> this.addTimeIntervals(TimeInterval.WEEK, 1)
+            TimeInterval.YEAR -> this.addTimeIntervals(TimeInterval.YEAR, 1)
+        }
+
+operator fun MyDate.plus(multipliedTimeInterval: MultipliedTimeInterval): MyDate =
+        this.addTimeIntervals(multipliedTimeInterval.interval, multipliedTimeInterval.multiplier)
+
+class MultipliedTimeInterval(val interval: TimeInterval, val multiplier: Int)
 
 fun MyDate.days(): Int {
     return year * 365 + month * 31 + dayOfMonth
@@ -18,7 +35,25 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+operator fun TimeInterval.times(multiplier: Int) : MultipliedTimeInterval = MultipliedTimeInterval(this, multiplier)
+
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return object : Iterator<MyDate> {
+
+            var currentDate: MyDate = start
+
+            override fun next(): MyDate {
+                return currentDate++
+            }
+
+            override fun hasNext(): Boolean {
+                return currentDate <= endInclusive
+            }
+
+        }
+    }
+}
 
 operator fun DateRange.contains(other: MyDate): Boolean =
         start < other && other <= endInclusive
